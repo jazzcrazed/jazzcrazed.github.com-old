dff07d887e29f13ef20163cc15d5a8da05f74a16
Marco Carag <pair+marco@howaboutwe.com>
2012-04-01 14:53:19 -0400

    adds about page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-/* Author:
-
-*/
-
-
-
-
-
+$(document).ready(function() {
+  var postPreviews = $('.postPreview');
+  if (postPreviews.length) {
+    setTimeout(function() {
+      postPreviews.each(function() {
+        console.log($(this), $(this).position());
+        if ($(this).position().left > 0) {
+          $(this).addClass('right');
+        }
+      });
+    }, 125);
+  }
+});
