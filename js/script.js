b2f59fc970f03bf34e68c4eb2f98c954ec4a2662
Marco Carag <me@marcocarag.com>
2012-12-20 12:02:44 -0500

    adds recent tweets to posts
    * upgrades to jquery 1.8.3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
-$(document).ready(function() {
-  var postPreviews = $('.postPreview');
-  if (postPreviews.length) {
+$(function() {
+  var $postPreviews = $('.postPreview'),
+      recentTweets, i,
+      url = encodeURIComponent('marcocarag.com/' + document.location.pathname);
+
+  if ($postPreviews.length) {
     setTimeout(function() {
-      postPreviews.each(function() {
-        console.log($(this), $(this).position());
+      $postPreviews.each(function() {
         if ($(this).position().left > 0) {
           $(this).addClass('right');
         }
       });
     }, 125);
+  }
+
+  recentTweets = {
+    '$container': $('#recentTweets'),
+    avatars: '',
+    populate: function(tweets) {
+      var tweet;
+
+      this.avatars = '<a href="http://twitter.com/search?q=' + url + '">';
+
+      for(i = 0; i < tweets.length; i++) {
+        tweet = tweets[i];
+
+        this.avatars = this.avatars +
+          '<img src="' + tweet.profile_image_url + '" alt="@' + tweet.from_user +
+          '" class="avatar" />'
+      }
+
+      this.avatars = this.avatars + '</a>';
+
+      this.$container.append(this.avatars).removeClass('hidden');
+    }
+  };
+
+  if (recentTweets.$container.length) {
+
+    $.ajax({
+      url:      'http://search.twitter.com/search.json?q=' + url + '&rpp=100',
+      dataType: 'jsonp',
+      success:  function(r) { recentTweets.populate(r.results); }
+    });
   }
 });
